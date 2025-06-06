3259f92973e3fbe5fb01e5c384801f3e84ebde93
Kirill Lappo <kirill-lappo@outlook.com>
2025-06-06 17:10:46 +0300

    corrected upload user image mutation types

--- src/HotChocolateDemo.Gql/Handlers/Users/Mutations/UpdateUsers/UpdateUserImageInput.cs
+++ src/HotChocolateDemo.Gql/Handlers/Users/Mutations/UpdateUsers/UpdateUserImageInput.cs
@@ -5,7 +5,6 @@ namespace HotChocolateDemo.Gql.Handlers.Users.Mutations.UpdateUsers;
 public class UpdateUserImageInput
 {
   [GraphQLNonNullType]
-  [ID<User>]
   public long UserId { get; set; }
 
   [GraphQLType(typeof(NonNullType<UploadType>))]
